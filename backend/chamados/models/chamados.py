71eb0349e427331377c3c0c274babe8bcfd3bca8
AlexConsig <alexsandro.medeiros@qualiconsig.com.br>
2025-03-25 10:01:56 -0300

    Adicionando migrations de chamados

--- backend/chamados/models/chamados.py
+++ backend/chamados/models/chamados.py
@@ -1,8 +1,7 @@
 from django.db import models
-from users.models import analista, Usuario
+from users.models.analista import Analista
+from users.models.usuarios import Usuario
 from .categoria import Categoria
-
-
 
 class Chamado(models.Model):
     titulo = models.CharField(max_length=200)
@@ -11,7 +10,7 @@ class Chamado(models.Model):
     gravidade = models.CharField(max_length=50)
     descricao = models.TextField()
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    analista = models.ForeignKey(analista, on_delete=models.CASCADE)
+    analista = models.ForeignKey(Analista, on_delete=models.CASCADE)
     criado_em = models.DateTimeField(auto_now_add=True)
     editado_em = models.DateTimeField(auto_now=True)
     encerrado_em = models.DateTimeField(null=True, blank=True)
