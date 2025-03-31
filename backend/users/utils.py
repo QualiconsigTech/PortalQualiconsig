72b9811d58f41dd362fa6ca52719d606a24fa5de
AlexConsig <alexsandro.medeiros@qualiconsig.com.br>
2025-03-31 17:55:11 -0300

    feat:8733706791 Adicionando resert de senha

---
+++ backend/users/utils.py
@@ -0,0 +1,25 @@
+from itsdangerous import URLSafeTimedSerializer
+from django.core.mail import send_mail
+from django.conf import settings
+from users.models.usuarios import Usuario
+
+def gerar_token_email(email):
+    serializer = URLSafeTimedSerializer(settings.SECRET_KEY)
+    return serializer.dumps(email, salt='reset-senha')
+
+def validar_token_email(token, tempo_expiracao=3600):
+    serializer = URLSafeTimedSerializer(settings.SECRET_KEY)
+    return serializer.loads(token, salt='reset-senha', max_age=tempo_expiracao)
+
+def enviar_email_reset_senha(email, token):
+    link = f"http://localhost:3000/redefinir-senha?token={token}" 
+    assunto = "Recuperação de senha"
+    mensagem = f"Clique no link para redefinir sua senha: {link}"
+
+    send_mail(
+        assunto,
+        mensagem,
+        settings.DEFAULT_FROM_EMAIL,
+        [email],
+        fail_silently=False,
+    )
