2dbc859906b88ec3b4f5ab16858e9878d8554117
AlexConsig <alexsandro.medeiros@qualiconsig.com.br>
2025-04-01 16:50:45 -0300

    Adicionando estrutura frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
-      "./src/**/*.{js,ts,jsx,tsx}", 
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+      "./src/layouts/**/*.{js,ts,jsx,tsx}",
+      "./src/app/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
     },
     plugins: [],
   };
-  
