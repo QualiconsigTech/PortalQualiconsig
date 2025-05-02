0f7a5ec027b2b0413f1d030e4b014a75ce0c00b2
JFernandoSan <jose.fernando@qualiconsig.com.br>
2025-05-02 16:29:36 -0300

    Melhorando Qlinks

--- backend/users/models/links.py
+++ backend/users/models/links.py
@@ -3,7 +3,7 @@ from django.db import models
 class links(models.Model):
     titulo = models.CharField(max_length=100)
     url = models.URLField()
-    tipo = models.CharField(max_length=20, default='sistema')
+    tipo = models.CharField(max_length=20,choices=[('sistema', 'Sistema'), ('banco', 'Banco')])
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.titulo
