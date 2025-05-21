4b07ac35e9c76bb51e56faf67e42fd64729fc6c9
AlexConsig <alexsandro.medeiros@qualiconsig.com.br>
2025-05-21 14:59:11 -0300

    Adicionando 1h de timeout

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5), 
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60), 
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
